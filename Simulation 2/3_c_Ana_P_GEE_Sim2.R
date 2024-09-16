cae783affea74252af5f5ac85adf329539907129
JulesPereiraMacedo <98019882+JulesPereiraMacedo@users.noreply.github.com>
2024-09-16 09:19:44 +0200

    3_c_Ana_P_GEE_Sim2.R

--- Simulation 2/3_c_Ana_P_GEE_Sim2.R
+++ Simulation 2/3_c_Ana_P_GEE_Sim2.R
@@ -46,11 +46,13 @@ Data_file = paste(Base_file,"Data_itt",sep = "/")
 # B_res_file_gee = paste(Resu_file_gee,"/Data_Output_binlogit",sep = "")
 P_res_file_gee = paste(Resu_file_gee,"/Data_Output_poisslog",sep = "")
 
-Time = 20
-
-
 # Vector of the scenarii ----
 Scen = rep(1:Nb_scen)
+
+Total_time_d = Sys.time()
+
+Time = 20
+Cor.FG = TRUE
 
 # Creation for each scenarii of the Excels ----
 for (n in Scen) {
@@ -74,29 +76,31 @@ for (n in Scen) {
   ## Empty files .csv ----
   
   write.table(name_poisslog,
-              file = paste(P_res_file_gee,"/Data_output_Poiss_log_Scenario_",n,".csv",sep = ""),
+              file = paste(P_res_file,"/Data_output_Poiss_log_Scenario_",n,".csv",sep = ""),
               row.names = FALSE,
               col.names = FALSE,
               sep = ";")
 }
 
 # Parallelism ----
-registerDoParallel(cores = 6)
+registerDoParallel(cores = Sys.getenv('SLURM_NTASKS'))
 
 itt = 1000
 
 for (n in Scen) {
   
   debut = Sys.time()
-  Scenario_use = get(paste("Scenario",n,sep="_")) 
+  
   
   res <- foreach(i = 1:itt,
                  # .combine = rbind, #unlist
-                 #.errorhandling = "remove", #s'il y a un problème enlève la ligne de
-                 .packages = c("stats","arm","gee","geepack","spind","doBy","doRNG","doParallel","dplyr","here","geesmv","matrixcalc")) %dorng% fun_cor_poiss(i=i,Time = Time,n=n,Base_file = Base_file,Workspace_name = Workspace_name,Data_file = Data_file,Resu_file = Resu_file_gee,Cor.FG=TRUE)  
+                 #.errorhandling = "remove", #s'il y a un problÃ¨me enlÃ¨ve la ligne de
+                 .packages = c("stats","arm","gee","geepack","spind","doBy","doRNG","doParallel","dplyr","here","geesmv","matrixcalc")) %dorng% fun_cor_poiss(i=i,Time = Time,n=n,Base_file = Base_file,Workspace_name = Workspace_name,Data_file = Data_file,Resu_file = Resu_file,Cor.FG=Cor.FG)
+  
   
   end = Sys.time()
   time_scen = end-debut
+  print(paste("Time to generated data from Scenario",n,sep = " : "))
   print(time_scen)
   
 }
@@ -107,7 +111,7 @@ for (n in Scen) {
 
 
 for (n in Scen) {
-  assign(paste("Data_Poiss_S",n,sep = "_"),read.csv2(paste(P_res_file_gee,"/Data_output_Poiss_log_Scenario_",n,sep = "",".csv")))
+  assign(paste("Data_Poiss_S",n,sep = "_"),read.csv2(paste(P_res_file,"/Data_output_Poiss_log_Scenario_",n,sep = "",".csv")))
   
   
   
@@ -134,7 +138,12 @@ for (n in Scen) {
       
       
       
-      write.table(res_poisslog,file = paste(P_res_file_gee,paste("Data_output_Poiss_log_Scenario_",sep = "",n,".csv"),sep = "/"),append = TRUE,row.names = FALSE,col.names = FALSE,sep = ";",dec = ",")
+      write.table(res_poisslog,file = paste(P_res_file,paste("Data_output_Poiss_log_Scenario_",sep = "",n,".csv"),sep = "/"),append = TRUE,row.names = FALSE,col.names = FALSE,sep = ";",dec = ",")
     }
   }
 }
+
+Total_time_e = Sys.time()
+Total_time = Total_time_e - Total_time_d 
+print("Total time for generating Data :")
+print(Total_time)
